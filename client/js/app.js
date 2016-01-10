4d318e62f146c6be7e393c5090edbb874c27f7b8
Celleb <tcelleb@gmail.com>
2016-01-10 21:09:01 +0200

    Just another one

--- client/js/app.js
+++ client/js/app.js
@@ -10,29 +10,57 @@
     var month = date.getMonth();
     var year = date.getFullYear();
     ndate = new Date(year, month, 1, 0, 0, 0, 0);
-    var running_day = function () {
-	return ndate.getDay();
-    }();
+    var running_day = ndate.getDay();
     var app = angular.module('oweek', ['ngMaterial']);
 
     app.controller('Calendar', function () {
 	this.wdays = wdays;
 	this.year = year;
 	this.month_name = moment(ndate).format("MMMM");
-	this.days = function () {
-	    last_day = moment(ndate).endOf('month').date();
-	    var days = [];
-	    var i;
-	    for (i = 1; i < running_day; i++) {
-		days.push(" ");
+	this.days = fillDays();
+	this.next = function () {
+	    monthAdd(1);
+	    this.year = year;
+	    ndate = new Date(year, month, 1, 0, 0, 0, 0);
+	    running_day = ndate.getDay();
+	    this.month_name = moment(ndate).format("MMMM");
+	    this.days = fillDays();
 
-	    }
-	    for (var j = 1; j <= last_day; j++) {
-		days.push(j);
-	    }
-	    return days;
-	}();
+	};
+
+	this.previous = function () {
+	    monthAdd(-1);
+	    this.year = year;
+	    ndate = new Date(year, month, 1, 0, 0, 0, 0);
+	    running_day = ndate.getDay();
+	    this.month_name = moment(ndate).format("MMMM");
+	    this.days = fillDays();
+
+	};
     });
 
+    function fillDays() {
+	last_day = moment(ndate).endOf('month').date();
+	var days = [];
+	var i;
+	for (i = 1; i < running_day; i++) {
+	    days.push(" ");
 
+	}
+	for (var j = 1; j <= last_day; j++) {
+	    days.push(j);
+	}
+	return days;
+    }
+
+    function monthAdd(add, scope) {
+	month += add;
+	if (month >= 12) {
+	    month = 0;
+	    year += 1;
+	} else if (month <= -1) {
+	    month = 11;
+	    year -= 1;
+	}
+    }
 })();
