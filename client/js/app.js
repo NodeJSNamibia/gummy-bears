624a7b54c2e110a544f0cb0a7e437fca27fa0b5d
Celleb <tcelleb@gmail.com>
2016-01-10 20:00:32 +0200

    Second Commit

--- client/js/app.js
+++ client/js/app.js
@@ -9,18 +9,21 @@
     var date = new Date();
     var month = date.getMonth();
     var year = date.getFullYear();
+    ndate = new Date(year, month, 1, 0, 0, 0, 0);
     var running_day = function () {
-	ndate = new Date(year, month, 1, 0, 0, 0, 0);
 	return ndate.getDay();
     }();
     var app = angular.module('oweek', ['ngMaterial']);
 
     app.controller('Calendar', function () {
+	this.wdays = wdays;
+	this.year = year;
+	this.month_name = moment(ndate).format("MMMM");
 	this.days = function () {
-	    last_day = moment().endOf('month').date();
+	    last_day = moment(ndate).endOf('month').date();
 	    var days = [];
 	    var i;
-	    for (i = 0; i <= running_day; i++) {
+	    for (i = 1; i < running_day; i++) {
 		days.push(" ");
 
 	    }
