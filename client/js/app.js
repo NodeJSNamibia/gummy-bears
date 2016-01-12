c9c663a8b38c2b7f1372938455d71d11fb465a0a
Celleb <tcelleb@gmail.com>
2016-01-12 12:31:25 +0200

    Creating cookie object to deal with cookies. Added Session Functionality

--- client/js/app.js
+++ client/js/app.js
@@ -1,9 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Angular JS Nust O'Week Application Module
+ * @author Celleb Tomanga <celleb@mrcelleb.com>
+ * @description text
+ * @contributors:
  */
-
 var wdays = ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
 var date = new Date();
 var month = date.getMonth();
@@ -25,10 +25,16 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 ]
 	);
 
-// API Service
+/**
+ * Handles all request to the API including authentication and Session Management.
+ * This is an angular service and should be injected in the controllers that need the service
+ * @author Celleb Tomanga <celleb@mrcelleb.com>
+ * @param {object} $http Angular JS http service object
+ * @param {object} $location Angular JS location service
+ */
 
 app.service('Api', ['$http', "$location", function ($http, $location) {
-
+	/* user object, authenticates the user and holds the user information */
 	var user = {
 	    authenticated: false,
 	    details: {},
@@ -41,6 +47,8 @@ app.service('Api', ['$http', "$location", function ($http, $location) {
 		});
 	    }
 	};
+
+	/* for test purposes only */
 	var userTest = {
 	    authenticated: false,
 	    details: {},
@@ -55,6 +63,7 @@ app.service('Api', ['$http', "$location", function ($http, $location) {
 			dept: "Department of Mechanical Engineering"
 		    };
 		    userTest.authenticated = true;
+		    cookie.setCookie("oweekSessionId", 1, 1);
 		    $location.path("/user");
 		} else {
 		    alert("Invalid Login");
@@ -62,11 +71,46 @@ app.service('Api', ['$http', "$location", function ($http, $location) {
 	    }
 	};
 	this.userTest = userTest;
+
+	/* user session functionality, reloads user information */
+	var session = {
+	    session_id: '',
+	    checkAuth: function () {
+		if (cookie.checkCookie("oweekSessionId")) {
+		    userTest.authenticated = true;
+
+		    session.session_id = cookie.getCookie("oweekSessionId");
+		    //check if session still exist on the server
+		    if (!1) {
+			// if session expired on server bring up login page
+			$location.path("/login");
+		    } else {
+			// populate user information
+			userTest.details = {
+			    name: "Field",
+			    surname: "Marshal",
+			    student_number: "201019593",
+			    faculty: "Faculty of Engineering",
+			    programme: "Beng Mechanical Engineering",
+			    dept: "Department of Mechanical Engineering"
+			};
+		    }
+		} else {
+		    this.userTest.authenticated = false;
+		}
+	    }
+	};
+	this.session = session;
     }]);
 var settings = {
     displayLogin: true
 };
-
+/**
+ * Calendar Logic
+ * @author Celleb Tomanga
+ * @param {type} param1
+ * @param {type} param2
+ */
 app.controller('Calendar', function () {
     this.wdays = wdays;
     this.year = year;
@@ -92,20 +136,35 @@ app.controller('Calendar', function () {
 
     };
 });
+
+
+/*
+ * function to generate calendar days of the month
+ * @author Celleb Tomanga
+ * @returns {Array|fillDays.days}
+ */
 function fillDays() {
     last_day = moment(ndate).endOf('month').date();
     var days = [];
     var i;
+    /* fill in empty days before the first day of the month */
     for (i = 1; i < running_day; i++) {
 	days.push(" ");
 
     }
+    /* fills in numbered days from the beginning of the month */
     for (var j = 1; j <= last_day; j++) {
 	days.push(j);
     }
     return days;
 }
 
+/**
+ * navigating calendar by month
+ * @author Celleb Tomanga
+ * @param {int} add "-1 to move to next month, +1 to advance to next month
+ * @returns {void}
+ */
 function monthAdd(add) {
     month += add;
     if (month >= 12) {
@@ -117,8 +176,14 @@ function monthAdd(add) {
     }
 }
 
-
+/**
+ * Angular JS Controller for the login partial
+ * Deals with authenitication by communicating with the Api service and login form control
+ * @author Celleb Tomanga
+ * @contributors
+ */
 app.controller('loginController', ['$scope', 'Api', function ($scope, Api) {
+	settings.displayLogin = false;
 	$scope.user = {
 	    number: '',
 	    pin: ''
@@ -127,7 +192,15 @@ app.controller('loginController', ['$scope', 'Api', function ($scope, Api) {
 	    Api.userTest.authenticate($scope.user.number, $scope.user.pin);
 	};
     }]);
-
+/**
+ * Angular JS Controller for the homePage
+ * Deals all features of the page outside other partials i.e header, footer
+ * It also integrates with the Api Service
+ * @author Celleb Tomanga
+ * @contributors
+ */
 app.controller("homeController", ["$scope", 'Api', function ($scope, Api) {
+	$scope.session = Api.session;
+	$scope.session.checkAuth()
 	$scope.api = Api.userTest;
     }]);
